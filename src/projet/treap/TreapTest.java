094ddac0ee3dcf8a44e254ffa6a516b005a7bf42
tuxayo <victor@tuxayo.net>
2014-10-09 13:51:50 +0200

    Refactor test, add assertions and find new bug

--- src/projet/treap/TreapTest.java
+++ src/projet/treap/TreapTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class TreapTest {
 
 	@Test
-	public void test() {
+	public void testSplit() {
 		Node<String> nodeYard = new Node<String>("yard", 0, 0);
 		Node<String> nodePied = new Node<String>("pied", 0, 0);
 		Node<String> nodeSillon = new Node<String>("sillon", 0, 0); // 0 because we don't need those values
@@ -16,6 +16,18 @@ public class TreapTest {
 		Treap<String, Integer> treap = new Treap<String, Integer>(nodeSillon);
 		Pair<Treap<String, Integer>, Treap<String, Integer>> splitResult = treap.split(treap.getNode(), "t");
 
-		assertEquals("sillon", splitResult.getFst().getNode().key);
+		Node<String> newNodeSillon = splitResult.getFst().getNode();
+		assertEquals("sillon", newNodeSillon.key);
+		assertEquals("pied", newNodeSillon.leftChild.key);
+		assertEquals(null, newNodeSillon.rightChild);
+
+		Node<String> newNodePied = newNodeSillon.leftChild;
+		assertEquals(null, newNodePied.leftChild);
+		assertEquals(null, newNodePied.rightChild);
+
+		Node<String> newNodeYard = splitResult.getSnd().getNode();
+		assertEquals("yard", newNodeYard.key); // sillon is here, WTF???
+//		assertEquals(null, newNodeYard.leftChild);
+//		assertEquals(null, newNodeYard.rightChild);
 	}
 }
